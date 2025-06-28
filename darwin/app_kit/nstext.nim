d94bde49baed46ec18a5b62ff50fb9d7d84c181b
gs-attripathi <attripathi@gainsight.com>
2025-06-28 17:19:07 +0530

    Update nstext.nim

--- darwin/app_kit/nstext.nim
+++ darwin/app_kit/nstext.nim
@@ -1,5 +1,6 @@
 import ./nsview
 import ../objc/runtime
-type NSText* = ptr object of NSView
+type 
+  NSText* = ptr object of NSView
 
 proc setString*(self: NSText ,str: NSString): void {.objc: "setString".}
